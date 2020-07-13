c8a0dce7e101fc81b73166c759c900939956cd87
Benjamin Chung <bwchung@andrew.cmu.edu>
2020-07-13 19:50:17 -0400

    Before refactoring tracksection

--- DVETCS/TrackSection.cs
+++ DVETCS/TrackSection.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using RangeTree;
 using UnityEngine;
 
 namespace DVETCS
@@ -22,6 +23,36 @@ namespace DVETCS
                 this.offset = offset;
             }
         }
+        public struct JunctionInfo
+        {
+            public readonly float position;
+            public readonly CrossingType crossing;
+            public readonly int? branch;
+            public JunctionInfo(float position, CrossingType crossing, int? branch)
+            {
+                this.position = position;
+                this.crossing = crossing;
+                this.branch = branch;
+            }
+        }
+        public class TrackUnit
+        {
+            public readonly TrackInfo info;
+            public Joiner next, prev;
+            public TrackUnit(TrackInfo info)
+            {
+                this.info = info;
+            }
+        }
+        public class Joiner
+        {
+            public readonly JunctionInfo info;
+            public Junction junction; public Junction.Branch branch;
+            public Joiner(JunctionInfo info)
+            {
+                this.info = info;
+            }
+        }
         public enum CrossingType
         {
             Forward,
@@ -29,37 +60,49 @@ namespace DVETCS
             Branch
         }
         private Dictionary<RailTrack, TrackInfo> tracks;
+        private Dictionary<Junction, JunctionInfo> junctions;
+        private RangeTree<float, TrackInfo> trackRanges;
         public TrackBlock(RailTrack baseTrack, bool direction)
         {
             tracks = new Dictionary<RailTrack, TrackInfo>();
-            tracks[baseTrack] = new TrackInfo(baseTrack, direction, 0.0);
+            trackRanges = new RangeTree<float, TrackInfo>();
+            var trackInfo = new TrackInfo(baseTrack, direction, 0.0);
+            tracks[baseTrack] = trackInfo;
+            junctions = new Dictionary<Junction, JunctionInfo>();
+            trackRanges.Add(0.0f, baseTrack.curve.length, trackInfo);
+            TrackAdded += delegate { };
+            TrackRemoved += delegate { };
+            JunctionAdded += delegate { };
+            JunctionRemoved += delegate { };
         }
 
-
-        private LinkedList<(RailTrack, bool, CrossingType, int?)> SearchFromtrack(RailTrack track)
+        private (LinkedList<(RailTrack track, bool direction, CrossingType crossing, Junction junction, int? branch)>, bool, RailTrack, Junction) SearchFromtrack(RailTrack track)
         {
             HashSet<(RailTrack, bool)> seen = new HashSet<(RailTrack, bool)>();
-            Queue<(RailTrack, bool, LinkedList<(RailTrack, bool, CrossingType, int?)>)> frontier = new Queue<(RailTrack, bool, LinkedList<(RailTrack, bool, CrossingType, int?)>)>();
-            frontier.Enqueue((track, true, new LinkedList<(RailTrack, bool, CrossingType, int?)>()));
-            frontier.Enqueue((track, false, new LinkedList<(RailTrack, bool, CrossingType, int?)>()));
+            Queue<(RailTrack, bool, Junction, LinkedList<(RailTrack, bool, CrossingType, Junction, int?)>)> frontier = new Queue<(RailTrack, bool, Junction, LinkedList<(RailTrack, bool, CrossingType, Junction, int?)>)>();
+            frontier.Enqueue((track, true, null, new LinkedList<(RailTrack, bool, CrossingType, Junction, int?)>()));
+            frontier.Enqueue((track, false, null, new LinkedList<(RailTrack, bool, CrossingType, Junction, int?)>()));
             while (frontier.Count > 0)
             {
-                var (ctrack, cdirection, history) = frontier.Dequeue();
+                var (ctrack, cdirection, cjunction, history) = frontier.Dequeue();
 
                 if (tracks.ContainsKey(ctrack))
-                    return history;
-
+                {
+                    Debug.Log($"njunct {String.Join(", ", history.Select(s=>s.Item4))} ctype {String.Join(", ", history.Select(s => s.Item3))}");
+                    return (history, cdirection, ctrack, cjunction);
+                }
+                
                 if (seen.Contains((ctrack, cdirection))) continue;
                 seen.Add((ctrack, cdirection));
-
+                
                 var nextJunction = cdirection ? ctrack.outJunction : ctrack.inJunction;
                 if (nextJunction == null)
                 {
                     var nextBranch = cdirection ? ctrack.outBranch : ctrack.inBranch;
-                    if (nextBranch == null) continue;
-                    var newList = new LinkedList<(RailTrack, bool, CrossingType, int?)>(history);
-                    newList.AddLast((ctrack, cdirection, CrossingType.Branch, null));
-                    frontier.Enqueue((nextBranch.track, nextBranch.track.inBranch.track == ctrack, newList));
+                    if (nextBranch == null || nextBranch.track == null) continue;
+                    var newList = new LinkedList<(RailTrack, bool, CrossingType, Junction, int?)>(history);
+                    newList.AddLast((ctrack, cdirection, CrossingType.Branch, cjunction, null));
+                    frontier.Enqueue((nextBranch.track, nextBranch.first, null, newList));
                     continue;
                 }
                 if (nextJunction.inBranch.track == ctrack) // in branch -> one of the out branches
@@ -68,9 +111,9 @@ namespace DVETCS
                     foreach (var branch in nextJunction.outBranches)
                     {
                         if (branch.track == null) continue;
-                        var newList = new LinkedList<(RailTrack, bool, CrossingType, int?)>(history);
-                        newList.AddLast((ctrack, cdirection, CrossingType.Forward, nbranch));
-                        frontier.Enqueue((branch.track, branch.track.inJunction == nextJunction, newList));
+                        var newList = new LinkedList<(RailTrack, bool, CrossingType, Junction, int?)>(history);
+                        newList.AddLast((ctrack, cdirection, CrossingType.Forward, nextJunction, nbranch));
+                        frontier.Enqueue((branch.track, branch.first, nextJunction, newList));
                         nbranch++;
                     }
                 }
@@ -78,50 +121,165 @@ namespace DVETCS
                 {
                     var next = nextJunction.inBranch;
                     if (next.track == null) continue;
-                    var newList = new LinkedList<(RailTrack, bool, CrossingType, int?)>(history);
-                    newList.AddLast((ctrack, cdirection, CrossingType.Backward, null));
-                    frontier.Enqueue((next.track, next.track.inJunction == nextJunction, newList));
+                    var newList = new LinkedList<(RailTrack, bool, CrossingType, Junction, int?)>(history);
+                    int nbranch = 0;
+                    foreach (var branch in nextJunction.outBranches)
+                    {
+                        if (branch.track == ctrack) break;
+                        nbranch++;
+                    }
+                    newList.AddLast((ctrack, cdirection, CrossingType.Backward, nextJunction, nbranch));
+                    frontier.Enqueue((next.track, next.first, nextJunction, newList));
                 }
             }
             throw new Exception("Invalid track configuration under train");
         }
 
+        private bool GetDirectionOfSourceTrack(RailTrack source, bool outDirection, CrossingType type)
+        {
+            Junction.Branch branch = null;
+            var junction = outDirection ? source.outJunction : source.inJunction;
+            switch (type)
+            {
+                case CrossingType.Branch:
+                    var nextDir = (outDirection ? source.outBranch : source.inBranch).first;
+                    var nextTrack = (outDirection ? source.outBranch : source.inBranch).track;
+                    return (nextDir ? nextTrack.inBranch : nextTrack.outBranch).first;
+                case CrossingType.Backward:
+                    return junction.outBranches.Where(b => b.track == source).Select(b => b.first).First();
+                case CrossingType.Forward:
+                    return junction.inBranch.first;
+            }
+            throw new Exception("Invalid junction type");
+        }
+
+        public void RemoveFrom(float position, bool removeDirection)
+        {
+            Debug.Log($"Removing from {position} in {removeDirection}");
+            foreach (var trk in tracks)
+            {
+                Debug.Log($"track {trk.Key.GetInstanceID()}@{trk.Value.offset} to {trk.Value.offset + trk.Key.curve.length} remove {(removeDirection ? trk.Value.offset >= position : trk.Value.offset + trk.Key.curve.length - float.Epsilon <= position)}");
+            }
+            foreach (var jn in junctions)
+            {
+                Debug.Log($"junction {jn.Key.GetInstanceID()} at {jn.Value.position} remove {(removeDirection ? jn.Value.position >= position + float.Epsilon : jn.Value.position - float.Epsilon <= position)}");
+            }
+            var rmTracks = tracks.Where(t => removeDirection ? t.Value.offset >= position : t.Value.offset + t.Key.curve.length - float.Epsilon <= position).ToArray();
+            var rmJunctions = junctions.Where(j => removeDirection ? j.Value.position >= position + float.Epsilon : j.Value.position - float.Epsilon <= position).ToArray();
+            foreach (var track in rmTracks)
+            {
+                tracks.Remove(track.Key);
+                trackRanges.Remove(track.Value);
+                TrackRemoved.Invoke(this, track.Value);
+            }
+            foreach (var junction in rmJunctions)
+            {
+                junctions.Remove(junction.Key);
+                JunctionRemoved.Invoke(this, junction.Key, junction.Value);
+            }
+        }
+
+        public void RemoveJunction(Junction junction, bool removeDirection)
+        {
+            if (!junctions.ContainsKey(junction)) return;
+            RemoveFrom(junctions[junction].position, removeDirection);
+        }
+
+        public void RemoveTrack(RailTrack track, bool removeDirection)
+        {
+            if (!tracks.ContainsKey(track)) return; // track does not exist in the current block
+            RemoveFrom((float)tracks[track].offset + (removeDirection ? 0.0f : (float)track.curve.length), removeDirection);
+        }
+
         public TrackInfo? AddTrack(RailTrack track)
+        {
+            return AddTrack(track, false);
+        }
+        public TrackInfo? AddTrack(RailTrack track, bool removeExisting)
         {
             if (tracks.ContainsKey(track))
                 return tracks[track];
-            var path = SearchFromtrack(track);
+            var (path, idir, incident, ijunction) = SearchFromtrack(track);
+            Debug.Log($"adding track {track.GetInstanceID()} ijunction {(ijunction ? ijunction.GetInstanceID() : -1 )}");
             if (path == null) return null;
 
             // we know that the tracks will appear in reverse order, so we walk "back" along the path
             var lastEl = path.Last.Value;
             var junction = (lastEl.Item2 ? lastEl.Item1.outJunction : lastEl.Item1.inJunction);
-            RailTrack pairedTo = null;
-            if (lastEl.Item3 == CrossingType.Forward)
-            {
-                pairedTo = junction.outBranches[lastEl.Item4.Value].track;
-            }
-            else if (lastEl.Item3 == CrossingType.Backward)
-            {
-                pairedTo = junction.inBranch.track;
-            }
-            else
-            {
-                pairedTo = lastEl.Item2 ? lastEl.Item1.outBranch.track : lastEl.Item1.inBranch.track;
-            }
+            RailTrack pairedTo = incident;
+
             var attachInfo = tracks[pairedTo];
-            var inDirection = ConnectedForward(pairedTo, lastEl.Item1);
-            var extendAction = inDirection == attachInfo.relativeDirection;
+            var extendAction = idir != attachInfo.relativeDirection;
             double offset = attachInfo.offset + (extendAction ? pairedTo.curve.length : 0.0);
-            foreach ((RailTrack, bool, CrossingType, int?) result in path.Reverse())
+
+            var connectTo = pairedTo;
+            if (removeExisting && ijunction != null && junctions.ContainsKey(ijunction))
             {
-                tracks[result.Item1] = new TrackInfo(result.Item1, extendAction ? !result.Item2:result.Item2, extendAction ? offset : offset - result.Item1.curve.length);
-                if (extendAction)
-                    offset += result.Item1.curve.length;
-                else
-                    offset -= result.Item1.curve.length;
+                JunctionRemoved.Invoke(this, ijunction, junctions[ijunction]);
+                junctions.Remove(ijunction);
             }
-            return tracks[path.Last.Value.Item1]; // we've just ensured that it exists
+            foreach ((RailTrack track, bool direction, CrossingType crossing, Junction junction, int? branch) result in path.Reverse())
+            {
+                var relDir = !GetDirectionOfSourceTrack(result.track, result.direction, result.crossing);
+                var computedOffset = extendAction ? offset : offset - result.track.curve.length;
+                Debug.Log($"EA {result.track.GetInstanceID()} {relDir != extendAction} {computedOffset} -> {computedOffset + result.track.curve.length} junction: {result.junction}");
+                var ti = new TrackInfo(result.track, relDir != extendAction, computedOffset);
+
+                if (removeExisting)
+                {
+                    var toRemove = trackRanges.Query((float)computedOffset + (extendAction?float.Epsilon:0.0f), (float)computedOffset + result.track.curve.length - (extendAction ? 0.0f : float.Epsilon));
+                    if (toRemove.Count() > 0)
+                    {
+                        Debug.Log($"rem cands {String.Join(", ", toRemove.Select(tr=>$"{tr.offset} -> {tr.offset + tr.track.curve.length}"))} for {computedOffset} to {computedOffset + result.track.curve.length}");
+                        var removeExtents = toRemove.Select(r => extendAction ? r.offset : r.offset + r.track.curve.length);
+                        var removeFrom = extendAction ? removeExtents.Min() : removeExtents.Max();
+                        Debug.Log("Remove action");
+                        RemoveFrom((float)removeFrom + (extendAction?1.0f:-1.0f), extendAction);
+                    }
+                }
+
+                Debug.Log($"extend 2 {ti.track.GetInstanceID()}");
+
+                tracks[result.track] = ti;
+                trackRanges.Add((float)ti.offset, (float)ti.offset + result.track.curve.length, ti);
+                if (result.junction != null)
+                {
+                    var junctionPos = (float)(offset);
+                    var ji = new JunctionInfo(junctionPos, result.crossing, result.branch);
+                    if (junctions.ContainsKey(result.junction))
+                        Debug.Log($"re-adding junction {result.junction.GetInstanceID()} at {junctions[result.junction].position} w/ track {result.track.GetInstanceID()}");
+                    else
+                        Debug.Log($"adding junction {result.junction.GetInstanceID()}");
+                    junctions.Add(result.junction, ji);
+                    JunctionAdded.Invoke(this, result.junction, ji);
+                }
+                Debug.Log($"extend 3");
+                TrackAdded.Invoke(this, ti, extendAction, result.junction);
+                pairedTo = result.track;
+                if (extendAction)
+                    offset += result.track.curve.length;
+                else
+                    offset -= result.track.curve.length;
+            }
+            Debug.Log($"extend 4 id {path.Last.Value.track.GetInstanceID()}");
+            return tracks[path.Last.Value.track]; // we've just ensured that it exists
+        }
+        
+
+        public TrackInfo? GetLastTrack()
+        {
+            float maxOffset = float.MinValue;
+            TrackInfo? output = null;
+            foreach (var ti in tracks)
+            {
+                var extent = ti.Value.offset + ti.Value.track.curve.length;
+                if (extent > maxOffset)
+                {
+                    maxOffset = (float)extent;
+                    output = ti.Value;
+                }
+            }
+            return output;
         }
 
         public static bool ConnectedForward(RailTrack from, RailTrack to)
@@ -156,10 +314,16 @@ namespace DVETCS
                     return nextJunction.outBranches[nextJunction.selectedBranch].track;
                 else
                     return nextJunction.outBranches[nextJunction.defaultSelectedBranch].track;
-            } else
+            }
+            else
             {
                 return nextJunction.inBranch.track;
             }
+        }
+
+        public static Junction JunctionAlongDir(RailTrack start, bool relativeDirection)
+        {
+            return relativeDirection ? start.outJunction : start.inJunction;
         }
 
         public TrackInfo? GetTrackInfo(RailTrack track)
@@ -187,6 +351,36 @@ namespace DVETCS
             return output;
         }
         
+        public bool HasPoint(float along)
+        {
+            return along < trackRanges.Max && along > trackRanges.Min; 
+        }
+        public (TrackInfo, float) GetSpan(float along)
+        {
+            var tracks = trackRanges.Query(along);
+            var track = tracks.First();
+            var span = (along - track.offset) / track.track.curve.length;
+            if (!track.relativeDirection) span = 1 - span;
+            return (track, (float)span);
+        }
+        public (Vector3 pos,Vector3 tang) CurveAt(float along)
+        {
+            var (track, span) = GetSpan(along);
+            return (pos: track.track.curve.GetPointAt(span), tang: track.track.curve.GetTangentAt(span));
+        }
+
+        public delegate void TrackAddedHandler(TrackBlock block, TrackInfo added, bool sideAdded, Junction via);
+        public event TrackAddedHandler TrackAdded;
+
+        public delegate void TrackRemovedHandler(TrackBlock block, TrackInfo removed);
+        public event TrackRemovedHandler TrackRemoved;
+
+        public delegate void JunctionAddedHandler(TrackBlock block, Junction added, JunctionInfo info);
+        public event JunctionAddedHandler JunctionAdded;
+
+        public delegate void JunctionRemovedHandler(TrackBlock block, Junction removed, JunctionInfo info);
+        public event JunctionRemovedHandler JunctionRemoved;
+
         public IEnumerator<TrackInfo> GetEnumerator()
         {
             return tracks.Values.GetEnumerator();
@@ -195,6 +389,14 @@ namespace DVETCS
         IEnumerator IEnumerable.GetEnumerator()
         {
             return tracks.Values.GetEnumerator();
+        }
+
+        public IEnumerable<KeyValuePair<Junction,JunctionInfo>> Junctions
+        {
+            get
+            {
+                return junctions.AsEnumerable();
+            }
         }
     }
 }
